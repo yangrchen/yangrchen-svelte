4d9df17bd1e1ae571544893b66ffb27922824022
Yang Chen <yyangc88@gmail.com>
2021-12-20 11:18:45 -0500

    Fixed about page links; added downloadable resume

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -2,7 +2,6 @@
     import { metatags, url } from "@roxi/routify";
     import ImageLoader from "../_components/Image/ImageLoader.svelte";
 
-    metatags.description = "Description coming soon...";
     let src = "images/yang-profile.webp";
 </script>
 
@@ -22,19 +21,26 @@
         >
             Hiya! I'm Yang, a software consultant and artist based in Chapel
             Hill. I love to design awesome projects for end users at <a
-                href={$url("/")}
+                href="https://www.macedontechnologies.com/"
+                target="_blank"
+                rel="noopener"
                 class="underline decoration-emerald-500">Macedon Technologies</a
             >. Outside of work, I'm an avid
-            <a href={$url("/resume")} class="underline decoration-cyan-500"
-                >representational artist</a
+            <a
+                href={$url("/artwork/drawings")}
+                class="underline decoration-cyan-500">representational artist</a
             >
             and a curious student of the online program at
-            <a href={$url("/artwork")} class="underline decoration-pink-500"
-                >Watts Atelier</a
+            <a
+                href="https://www.wattsatelier.com/"
+                target="_blank"
+                rel="noopener"
+                class="underline decoration-pink-500">Watts Atelier</a
             >
             Check out my work in the
-            <a href={$url("/artwork")} class="underline decoration-cyan-500"
-                >gallery</a
+            <a
+                href={$url("/artwork/drawings")}
+                class="underline decoration-cyan-500">gallery</a
             >!
         </p>
         <p>Feel free to ping me by email or social media!</p>
--- src/pages/resume/index.svelte
+++ src/pages/resume/index.svelte
@@ -1,10 +1,20 @@
 <script>
     import WorkTile from "../../_components/WorkTile/WorkTile.svelte";
     import jobs from "../../lib/data/jobs.json";
+    let href = "/public/yangrchen_resume_fall_2021_120521.pdf";
 </script>
 
 <div class="mt-6 md:mt-10">
     <h1 class="text-center text-4xl font-bold">Work</h1>
+    <div>
+        <a
+            {href}
+            target="_blank"
+            rel="noopener"
+            class="underline decoration-gray-300 hover:decoration-pink-300"
+            >Resume Link</a
+        >
+    </div>
     <div class="mt-8 space-y-8">
         {#each jobs as job}
             <WorkTile {...job} />
